d549bcc409677baca323e6e461b65ac5b61efe36
andrasatriapratama <andrasatriapratama@gmail.com>
2022-09-14 22:40:52 +0700

    gulp and css

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,15 @@
-function defaultTask(cb) {
-    // place code for your default task here
-    console.log('test');
-    cb();
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+const cleanCSS = require('gulp-clean-css');
+const sourcemaps = require('gulp-sourcemaps');
+
+function css() {
+    return gulp.src('dev/css/*')
+        .pipe(sourcemaps.init())
+        .pipe(concat('style.css'))
+        .pipe(cleanCSS())
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('dist/css'))
 }
 
-exports.default = defaultTask
+exports.default = css
